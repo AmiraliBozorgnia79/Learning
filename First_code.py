78a79258d8734dd4c8a8431358e474cc8daaa81e
AmiraliBozorgnia79 <amiralibozorgnia79@gmail.com>
2024-03-17 23:59:27 -0400

    This is the second change

--- First_code.py
+++ First_code.py
@@ -1,6 +1,15 @@
-print("Hello")
-a=1
-if a<1:
-    print("a is less than 1")
+# Class_Ex1:
+# Write python program that converts seconds to
+# (x Hour, x min, x seconds)
+# ----------------------------------------------------------------
+
+S=int(input('Please enter seconds:'))
+if S>=3600:
+    H=S//3600
+    M=((S%3600)//60)
+    Sec=((S%3600)%60)
+    print('Hours:',H,'Minutes:',M,'Seconds:',Sec)
 else:
-    print("a is greater than 1")
+    print('Hours:', '0', 'Minutes:', S//60, 'Seconds:', S%60)
+exit()
+#-----------------------------------------------------------
