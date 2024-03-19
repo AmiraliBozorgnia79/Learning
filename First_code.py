6c0bde7ffdebabb0be6c27adef256fee9b7c2b23
AmiraliBozorgnia79 <amiralibozorgnia79@gmail.com>
2024-03-19 00:34:44 -0400

    This is the second change

--- First_code.py
+++ First_code.py
@@ -3,13 +3,41 @@
 # (x Hour, x min, x seconds)
 # ----------------------------------------------------------------
 
-S=int(input('Please enter seconds:'))
-if S>=3600:
-    H=S//3600
-    M=((S%3600)//60)
-    Sec=((S%3600)%60)
-    print('Hours:',H,'Minutes:',M,'Seconds:',Sec)
-else:
-    print('Hours:', '0', 'Minutes:', S//60, 'Seconds:', S%60)
-exit()
+#S=int(input('Please enter seconds:'))
+#if S>=3600:
+ #   H=S//3600
+  #  M=((S%3600)//60)
+   # Sec=((S%3600)%60)
+    #print('Hours:',H,'Minutes:',M,'Seconds:',Sec)
+#else:
+ #   print('Hours:', '0', 'Minutes:', S//60, 'Seconds:', S%60)
+#exit()
 #-----------------------------------------------------------
+#My_Practice
+
+name="Amirali Bozorgnia"
+print(len(name))
+print(name.split("Amirali Bozorgnia"))
+print(name.find("a"))
+print(name.upper())
+print(name.replace("Amirali Bozorgnia", "Hossein Tabary"))
+print(name)
+
+import math
+k=3.569
+print(round(k,2))
+print(round(k,3))
+print(round(k,1))
+print(math.log10(k))
+print(math.floor(k))
+print(math.ceil(k))
+print(math.sin(k))
+print(math.sqrt(k))
+print(pow(k,2))
+
+name="Amirali Bozorgnia"
+First_name=name[0:7]
+last_name=name[8:]
+print(First_name+" "+last_name)
+print(name[0:7:2])
+
