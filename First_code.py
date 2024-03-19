260b0002b11b799c31c9deb61f140b85819751d0
AmiraliBozorgnia79 <amiralibozorgnia79@gmail.com>
2024-03-19 18:15:08 -0400

    This is my code for python practicing

--- First_code.py
+++ First_code.py
@@ -12,11 +12,53 @@
 #else:
  #   print('Hours:', '0', 'Minutes:', S//60, 'Seconds:', S%60)
 #exit()
+#-----------------------------------------------------------------
+#write a program to print all the even numbers from 1 to 100
+#for i in range(1,100):
+ #if i%2==0:
+  # print(i,end=",")
 #-----------------------------------------------------------
+#write a program to find the factrial of the given number
+#j=int(input("please enter your number :"))
+#factorial=1
+#if j<0:
+ #   print("it cannot be calculated")
+#elif j==0:
+ #   print("the factorial of zero is one")
+#else:
+ #   while j>0:
+  #   factorial=factorial*j
+   #  j=j-1
+
+#print("the factrial of your number is ",factorial)
+#---------------------------------------------------------
+#Write a program that calculates the sum of the digits of a given number
+#T=int(input("Enter the number"))
+#n1=T%10
+#n10=(T//10)%10
+#n100=(T//100)
+
+#print(" sum of your digits is: ", n1+n10+n100)
+#-----------------------------------------------------------
+#Write a program that calculates the sum of the first N natural numbers, where N is a positive integer provided by the user.
+#k=int(input("please enter your number: "))
+
+base=0
+
+#if k<0:
+ #   print("sorry, I cannot process negative number")
+#elif k==0:
+ #   print("sorry, I cannot process zero")
+#else:
+  #  while k>0:
+   #     base=base+k
+    #    k=k-1
+#print("the sum of your first N natural numbers is :", base)
+#-------------------------------------------------------------
 #My_Practice
 
 name="Amirali Bozorgnia"
-print(len(name))
+#print(len(name))
 print(name.split("Amirali Bozorgnia"))
 print(name.find("a"))
 print(name.upper())
